83b5473dae1ed69279fdb4e1de8d96d6d1aef884
limbaba1120 <kwlim1120ss@gmail.com>
2024-09-27 10:16:54 +0900

    feat: java study week2 assignment - array - inheritance

---
+++ src/main/java/org/java/chapter3/ch18/ObjectCopy.java
@@ -0,0 +1,38 @@
+package org.java.chapter3.ch18;
+
+public class ObjectCopy {
+	public static void main(String[] args) {
+		Book[] library = new Book[4];
+		Book[] copyLibrary = new Book[4];
+
+		library[0] = new Book("태백산맥1", "조정");
+		library[1] = new Book("태백산맥2", "조정");
+		library[2] = new Book("태백산맥3", "조정");
+		library[3] = new Book("태백산맥4", "조정");
+
+		System.arraycopy(library, 0, copyLibrary, 0, library.length);
+
+		for (Book book : library) {
+			book.showBookInfo();
+		}
+
+		System.out.println("==============");
+
+		for (Book book : copyLibrary) {
+			book.showBookInfo();
+		}
+
+		library[0].setAuthor("박원서");
+		library[0].setTitle("나목");
+
+		for (Book book : library) {
+			book.showBookInfo();
+		}
+
+		System.out.println("==============");
+
+		for (Book book : copyLibrary) {
+			book.showBookInfo();
+		}
+	}
+}
---
+++ src/main/java/org/java/chapter5/ch03/Customer.java
@@ -0,0 +1,76 @@
+package org.java.chapter5.ch03;
+
+public class Customer {
+	protected int customerId;
+	protected String customerName;
+	protected String customerGrade;
+
+	int bonusPoint;
+	double bonusRatio;
+
+/*	public Customer() {
+		customerGrade = "SILVER";
+		bonusRatio = 0.01;
+		System.out.println("CUSTOMER() CALL");
+	}*/
+
+	public Customer(int customerId, String customerName) {
+		this.customerId = customerId;
+		this.customerName = customerName;
+
+		customerGrade = "SILVER";
+		bonusRatio = 0.01;
+		System.out.println("CUSTOMER(int, String) CALL");
+
+	}
+
+	public int calcPrice(int price) {
+		bonusPoint += price * bonusRatio;
+		return price;
+	}
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getCustomerGrade() {
+		return customerGrade;
+	}
+
+	public void setCustomerGrade(String customerGrade) {
+		this.customerGrade = customerGrade;
+	}
+
+	public int getBonusPoint() {
+		return bonusPoint;
+	}
+
+	public void setBonusPoint(int bonusPoint) {
+		this.bonusPoint = bonusPoint;
+	}
+
+	public double getBonusRatio() {
+		return bonusRatio;
+	}
+
+	public void setBonusRatio(double bonusRatio) {
+		this.bonusRatio = bonusRatio;
+	}
+
+	public String showCustomerInfo() {
+		return customerName + "님의 등급은" + customerGrade +
+			"이며, 보너스 포인트는" + bonusPoint + "입니다";
+	}
+}
